b881e47327714393a53e7c9d42d9b0e90dbea71f
Aneesh Tigga <aneeshtigga@gmail.com>
2017-01-12 22:59:32 +0530

    Add files via upload

---
+++ gdt.h
@@ -0,0 +1,6 @@
+#ifndef __GDT_H
+#define __GDT_H
+
+	class GlobalDescriptorTable
+	{
+
--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,8 @@
+#include "types.h"
+
 void printf(char* str)
 {
-	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 	for(int i=0; str[i] != '\0'; ++i)
 		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
@@ -8,7 +10,7 @@ void printf(char* str)
 extern "c" constructor start_ctors;
 extern "c" constructor end_ctors;
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t /*magicnumber*/)
 {
 	printf("Hello Aneesh! --- http://www.google.com");
 
